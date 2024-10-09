19fcf2f775513e005d98d5040f0c4cf99ee20e13
Pablo Albaladejo <pablo.albaladejo.mestre@gmail.com>
2024-10-09 23:22:08 +0200

    chore: refactor infra stack

--- infra/lib/frontend-stack.ts
+++ infra/lib/frontend-stack.ts
@@ -2,7 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { StaticSite } from './static-site';
 
-export class InfrastructureStack extends cdk.Stack {
+export class FrontendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
--- infra/test/frontend.test.ts
+++ infra/test/frontend.test.ts
@@ -1,10 +1,10 @@
 import * as cdk from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
-import * as Infrastructure from '../lib/infrastructure-stack';
+import * as Infrastructure from '../lib/frontend-stack';
 
 test('CloudFront Distribution Created', () => {
   const app = new cdk.App();
-  const stack = new Infrastructure.InfrastructureStack(app, 'MyTestStack');
+  const stack = new Infrastructure.FrontendStack(app, 'MyTestStack');
   const template = Template.fromStack(stack);
   template.hasResourceProperties('AWS::CloudFront::Distribution', {});
 });
