547ce9156b9a253a4490bbcbb38899d5f3f21eaa
Pablo Albaladejo <pablo.albaladejo.mestre@gmail.com>
2024-10-09 18:23:36 +0200

    chore: add linters

---
+++ apps/frontend/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  extends: [
+    '@my-org/eslint-config',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: ['react', 'react-hooks'],
+  rules: {
+    'react/prop-types': 'off',
+  },
+};
---
+++ packages/eslint-config/index.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+};
